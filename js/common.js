d8a1ec5381602f274697f159ec61243d19a565f9
KimForest91 <forest_91@naver.com>
2023-05-25 11:09:29 +0900

    변경사항 저장

--- js/common.js
+++ js/common.js
@@ -1,3 +1,34 @@
-() => {
+/* let a = 'test test';
+let b = '테스트테스트';
 
-}
+// export default a; 한개만 내보내기
+export {a};
+export {b};
+
+
+
+
+
+
+
+
+
+
+
+var myWorker = new Worker('worker.js'); 
+
+w.onmessage = function(e){
+  console.log(e.data) //이러면 1 나올듯
+};
+
+
+
+
+
+
+
+
+
+
+
+ */
---
+++ js/worker.js
@@ -0,0 +1,2 @@
+/* var i = 0;
+postMessage(i + 1); //postMessage라는 특별한 함수가 있음 */
